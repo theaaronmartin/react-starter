b5cfbb85a250f43b42fa61433c1da2ecb5e4add9
Rush Tehrani <rush.tehrani@gmail.com>
2016-08-11 06:27:02 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+      path = require('path'),
+      logger = require('morgan');
+
+const app = express();
+
+// Middleware
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+
+// Run application
+app.listen(3001, function() {
+  console.log('Blogger web is running!');
+});
