d1d6e04467a2bdda751fce2559df1ee723daa059
Aaron Martin <grav3serker@Aarons-iMac.local>
2016-08-15 14:30:16 -0700

    adding css

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,63 @@ class CommentList extends React.Component {
 }
 
 class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { user: { username: '' }, body: '' };
+
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handleBodyChange = this.handleBodyChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+    handleUsernameChange(e) {
+      this.setState({ user: { username: e.target.value } });
+    }
+
+    handleBodyChange(e) {
+      this.setState({ body: e.target.value });
+    }
+
+    handleSubmit(e) {
+      e.preventDefault();
+
+      var username = this.state.user.username.trim();
+      var body = this.state.body.trim();
+
+      if (!username || !body) {
+        return;
+      }
+
+      this.props.onCommentSubmit({
+        user: {
+          username: username
+        },
+        body: body
+      });
+
+      this.setState({ user: { username: '' }, body: '' });
+    }
+
   render() {
     return (
-      <div className="comment-form">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="comment-form" onSubmit={this.handleSubmit}>
+        <input
+            className="text-box"
+            type="text"
+            placeholder="Your username"
+            value={this.state.user.username}
+            onChange={this.handleUsernameChange}
+        />
+        <input
+            className="text-box"
+            type="text"
+            placeholder="Say something..."
+            value={this.state.body}
+            onChange={this.handleBodyChange}
+        />
+          <input type="submit" value="Post" />
+      </form>
     );
   }
 }
@@ -44,6 +96,7 @@ class CommentBox extends React.Component {
 
     this.state = { data: { comments: [] } };
     this.getComments = this.getComments.bind(this);
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
 }
 
 getComments() {
@@ -60,9 +113,25 @@ getComments() {
   }.bind(this));
 }
 
+handleCommentSubmit(comment) {
+  $.ajax({
+      url: this.props.url + '/comments/',
+      dataType: 'json',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(comment)
+    })
+    .done(function(post) {
+      this.setState({data: post});
+    }.bind(this))
+    .fail(function(xhr, status, err) {
+      console.error(err);
+    }.bind(this));
+}
+
 componentDidMount() {
     this.getComments();
-    setInterval(this.getComments, this.props.pollInterval);
+    // setInterval(this.getComments, this.props.pollInterval);
   }
 
   render() {
@@ -70,7 +139,7 @@ componentDidMount() {
       <div className="comment-box">
         <h1 className="cb-title">C<span>omment</span>s</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
